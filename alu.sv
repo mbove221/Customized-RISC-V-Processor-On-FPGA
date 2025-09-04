ff699a61a94b17ce4dc4a700a5d68c77832e5a62
mbove221 <mbove221@gmail.com>
2025-09-04 10:54:48 -0400

    Updated alu skeleton

--- alu.sv
+++ alu.sv
@@ -3,11 +3,15 @@
 * @input[31:0] alu_in2 - second 32-bit input register to alu
 * @input[3:0] alu_op_ctrl - 4-bit ALU control signal (specify what operation
 * to perform). alu_op_ctrl signal is based on ALU control unit
+* @output[31:0] alu_out - 32-bit output register
+* @output zero - 1-bit control signal used for branch instructions
 */
+
 module alu(input [31:0] alu_in1,
 	input [31:0] alu_in2,
 	input [3:0] alu_op_ctrl,
-	output logic[31:0] alu_out
+	output logic[31:0] alu_out,
+	output logic zero
 	);
 
 	always_comb begin
