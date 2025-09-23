ea7a38b66e6bca2cbd1c2dc4a14de5c57d7cca44
johnabr01 <johnaby@yahoo.com>
2025-09-23 19:22:24 -0400

    reg file and top-level changes

--- reg_file.sv
+++ reg_file.sv
@@ -2,9 +2,7 @@
 module regfile_ff #(
   parameter N = 32,            // number of registers
   parameter W = 32,            // width
-  parameter ZERO_REG = 0       // index of zero register (set to 0 for RISC-V)
 ) (
-  input  logic                  clk,
   input  logic                  wen,
   input  logic [$clog2(N)-1:0]  waddr, // clog2 is ceiling of log2 of N
   input  logic [W-1:0]          wdata,
@@ -17,37 +15,10 @@ module regfile_ff #(
   // declare register array as FFs
   logic [W-1:0] regs [0:N-1];
 
-  // write (on posedge)
-  always_ff @(posedge clk) begin
-    if (wen && (waddr != ZERO_REG)) begin
-      regs[waddr] <= wdata;
-    end
-  end
-
-  // combinational (asynchronous) reads
-  // forward read when reading the register being written in same cycle (optional here)
   always_comb begin
-    // default direct read (combinational)
-    if (wen && (waddr == raddr1) && (waddr != ZERO_REG))
-      rdata1 = wdata;            // simple write-forwarding
-    else if (raddr1 == ZERO_REG)
-      rdata1 = '0;
-    else
-      rdata1 = regs[raddr1];
-
-    if (wen && (waddr == raddr2) && (waddr != ZERO_REG))
-      rdata2 = wdata;
-    else if (raddr2 == ZERO_REG)
-      rdata2 = '0;
-    else
-      rdata2 = regs[raddr2];
+    if (wen) regs[waddr] <= wdata;
+    rdata1 = regs[raddr1];
+    rdata2 = regs[raddr2];
   end
-
-  // idk if this is going to work but ive put it here just in case, we think of doing it.
-  // optional power-up init to zero (synthesis may or may not support)
-  // initial begin
-  //   integer i;
-  //   for (i=0;i<N;i=i+1) regs[i] = '0;
-  // end
 
 endmodule
