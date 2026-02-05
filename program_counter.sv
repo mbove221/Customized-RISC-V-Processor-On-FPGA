b303faeb38e9dda57217e9d9335504040fd05fd5
Michael Bove <mbove@lab27.ece.stonybrook.edu>
2026-02-05 14:34:55 -0500

    Updated program counter to stop at end of program

--- program_counter.sv
+++ program_counter.sv
@@ -2,14 +2,15 @@ module program_counter(
     input clk,
     input reset_n,
     input [31:0] pc_next,
+    input processor_done,
     output logic [31:0] pc
 );
 
 always_ff @(posedge clk) begin
     if(reset_n == 1'b0) begin
         pc <= 32'b0;
-    end else begin
-        pc <= pc_next;
+    end else if(processor_done != 1'b1) begin
+        pc <= 32'b0;
     end
 
 end
